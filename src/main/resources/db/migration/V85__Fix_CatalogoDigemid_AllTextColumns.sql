abf6899a678092471928d831981d588fd5310b8d
joaquinct11 <joaquincastillotello2001@gmail.com>
2026-08-01 15:28:17 -0500

    add digemid and fix cantidad productos

---
+++ src/main/resources/db/migration/V85__Fix_CatalogoDigemid_AllTextColumns.sql
@@ -0,0 +1,7 @@
+ALTER TABLE catalogo_digemid
+    ALTER COLUMN cod_prod       TYPE TEXT,
+    ALTER COLUMN nom_prod       TYPE TEXT,
+    ALTER COLUMN presentac      TYPE TEXT,
+    ALTER COLUMN num_reg_san    TYPE TEXT,
+    ALTER COLUMN nom_titular    TYPE TEXT,
+    ALTER COLUMN nom_fabricante TYPE TEXT;
